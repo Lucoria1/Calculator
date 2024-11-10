cd789cd738675a79f66235d39eff9c55ec805dce
Lucoria <183291283+Lucoria1@users.noreply.github.com>
2024-11-10 16:47:35 +0000

    added multiple functionalities

--- calculator.js
+++ calculator.js
@@ -1,10 +1,73 @@
 window.onload =() => {
+
+let numberOne = "";
+let numberTwo = "";
+let Operator = "";
+let display = "";
+let displayOp = display;
+let displayCalculation = "";
+let displayResult = "";
+
+function operate (a, b, c) {
+    if (b === "+"){return add(a,c)}
+    if (b === "-"){return subtract(a,c)}
+    if (b === "*"){return multiply(a,c)}
+    if (b === "/"){return divide(a,c)}
+}
     
-Array.from(document.querySelectorAll(".btn")).map((event) => {
-    event.addEventListener("click", () => {
-        console.log("click")
+Array.from(document.querySelectorAll(".num")).map((event) => {
+    event.addEventListener("click", (event) => {
+        if(Operator === ""){
+            numberOne += event.srcElement.className[8]
+            display = numberOne;
+            document.querySelector(".results").innerHTML = display;
+            console.log(numberOne)}
     })
+});
+
+Array.from(document.querySelectorAll(".op")).map((event) => {
+    event.addEventListener("click", (event) => {
+        Operator = event.srcElement.className[7]
+        displayOp = display + Operator
+        document.querySelector(".results").innerHTML = displayOp
+        console.log(Operator)
+    })
+});
+
+Array.from(document.querySelectorAll(".num")).map((event) => {
+    event.addEventListener("click", (event) => {
+        if(Operator !== ""){
+            numberTwo += event.srcElement.className[8]
+            displayCalculation = numberOne + Operator + numberTwo
+            document.querySelector(".results").innerHTML = displayCalculation
+            console.log(displayCalculation)}
+    })
+});
+
+document.querySelector(".calculate").addEventListener("click", () => {
+    displayResult = operate(numberOne, Operator, numberTwo);
+    document.querySelector(".results").innerHTML = displayResult
 })
+
+document.querySelector(".clear").addEventListener("click", () => {
+    numberOne = "";
+    numberTwo = "";
+    Operator = "";
+    display = "";
+    displayOp = display;
+    displayCalculation = "";
+    displayResult = "";
+    document.querySelector(".results").innerHTML = display;
+})
+
+function add (a, b){return a + b};
+
+function subtract(a, b){return a - b};
+
+function multiply(a, b){return a * b};
+
+function divide(a, b){return a / b};
+
 
 
 }
